a480e1e2e1e62ba78451d2edee91c27cf20b5737
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-02-13 09:17:39 -0800

    Done.

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class AddSeasonToShows < ActiveRecord::Migration[5.2]
+    
+    
+    def change
+
+        add_column :shows, :season, :string
+        
+    end
+    
+end
+# binding.pry
+
+
